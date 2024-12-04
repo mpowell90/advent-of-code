ac5ab7eb14e1b3fde7905207d84b7fdeeaa3a799
Mat <mattpowell90@gmail.com>
2024-12-04 01:48:20 +0000

    feat: WIP: attempt

--- 2024/day-2/src/main.rs
+++ 2024/day-2/src/main.rs
@@ -1,3 +1,15 @@
+use std::collections::VecDeque;
+
+fn main() {
+    let input = include_str!("./input.txt");
+
+    let safe_report_count_part_1: usize = safe_report_count_part_1(input);
+    println!("Safe report count part 1: {}", safe_report_count_part_1);
+
+    let safe_report_count_part_2: usize = safe_report_count_part_2(input);
+    println!("Safe report count part 2: {}", safe_report_count_part_2);
+}
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum Direction {
     None,
@@ -17,7 +29,7 @@ impl Direction {
 
 #[derive(Clone, Debug)]
 struct Report {
-    pub levels: Vec<isize>,
+    pub levels: VecDeque<isize>,
 }
 
 impl Report {
@@ -66,31 +78,88 @@ impl Report {
 
         false
     }
+
+    pub fn is_safe_with_problem_dampener(&self) -> bool {
+        let mut last_level: Option<isize> = None;
+        let mut problems = 0;
+
+        let mut levels = self.levels.clone();
+
+        while let Some(level) = levels.front() {
+            let Some(last) = last_level else {
+                last_level = Some(*level);
+                levels.pop_front();
+                continue;
+            };
+            
+            dbg!(last);
+            dbg!(level);
+
+            let change = last - level;
+            dbg!(last - level);
+            dbg!(change.abs());
+            
+            if !(1..=3).contains(&change.abs()) || last.signum() != level.signum() {
+                problems += 1;
+            }
+
+            // if last.signum() != level.signum() {
+            //     problems += 1;
+            //     is_problem = true;
+            // }
+            
+            dbg!(problems);
+
+            last_level = Some(*level);
+            
+            levels.pop_front();
+        }
+
+        if problems > 1 {
+            return false;
+        }
+
+        true
+    }
 }
 
-fn main() {
-    let input = include_str!("./input.txt");
-
-    let safe_report_count: usize = input
+pub fn safe_report_count_part_1(input: &str) -> usize {
+    input
         .lines()
         .map(|line| Report::parse(line).is_safe())
         .filter(|&is_safe| is_safe)
-        .count();
+        .count()
+}
 
-    println!("Safe report count: {}", safe_report_count);
+pub fn safe_report_count_part_2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| Report::parse(line).is_safe_with_problem_dampener())
+        .filter(|&is_safe| is_safe)
+        .count()
 }
 
 #[cfg(test)]
 mod tests {
     use crate::Report;
 
+    // #[test]
+    // fn should_check_report_safety_part_1() {
+    //     assert!(Report::parse("7 6 4 2 1").is_safe());
+    //     assert!(!Report::parse("1 2 7 8 9").is_safe());
+    //     assert!(!Report::parse("9 7 6 2 1").is_safe());
+    //     assert!(!Report::parse("1 3 2 4 5").is_safe());
+    //     assert!(!Report::parse("8 6 4 4 1").is_safe());
+    //     assert!(Report::parse("1 3 6 7 9").is_safe());
+    // }
+
     #[test]
-    fn should_check_report_safety() {
-        assert!(Report::parse("7 6 4 2 1").is_safe());
-        assert!(!Report::parse("1 2 7 8 9").is_safe());
-        assert!(!Report::parse("9 7 6 2 1").is_safe());
-        assert!(!Report::parse("1 3 2 4 5").is_safe());
-        assert!(!Report::parse("8 6 4 4 1").is_safe());
-        assert!(Report::parse("1 3 6 7 9").is_safe());
+    fn should_check_report_safety_part_2() {
+        // assert!(Report::parse("7 6 4 2 1").is_safe_with_problem_dampener());
+        assert!(!Report::parse("1 2 7 8 9").is_safe_with_problem_dampener());
+        // assert!(!Report::parse("9 7 6 2 1").is_safe_with_problem_dampener());
+        // assert!(Report::parse("1 3 2 4 5").is_safe_with_problem_dampener());
+        // assert!(Report::parse("8 6 4 4 1").is_safe_with_problem_dampener());
+        // assert!(Report::parse("1 3 6 7 9").is_safe_with_problem_dampener());
     }
 }
